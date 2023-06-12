2e91a241e897c59e64207319a5b1fd45665a5482
Finn Bear <finnbearlabs@gmail.com>
2023-06-12 12:13:31 -0700

    Implement TAPIR (WIP).

--- src/occ/store.rs
+++ src/occ/store.rs
@@ -1,8 +1,9 @@
 use super::Transaction;
 use crate::MvccStore;
 use std::collections::hash_map::Entry;
-use std::collections::{BTreeSet, HashSet};
+use std::collections::{BTreeMap, BTreeSet, HashSet};
 use std::hash::Hash;
+use std::ops::Bound;
 use std::{borrow::Borrow, collections::HashMap};
 
 pub(crate) struct Store<K, V, TS> {
@@ -66,7 +67,80 @@ impl<K: Eq + Hash, V, TS: Copy + Ord> Store<K, V, TS> {
             let Some((beginning, end)) = self.inner.get_range(key, *read) else {
                 continue;
             };
+
+            // If we don't have this version then no conflicts for read.
+            if beginning != *read {
+                continue;
+            }
+
+            if let Some(end) = end {
+                // Value is now invalid (not the latest version).
+                if self.linearizable || commit > end {
+                    return PrepareResult::Fail;
+                } else {
+                    // There may be a pending write in the past.
+                    if let Some(writes) = prepared_writes.get(key) {
+                        if writes
+                            .range((Bound::Excluded(*read), Bound::Excluded(commit)))
+                            .next()
+                            .is_some()
+                        {
+                            // Read conflicts with later prepared write.
+                            return PrepareResult::Abstain;
+                        }
+                    }
+                }
+            } else {
+                // The value is still valid (the latest version).
+                if let Some(writes) = prepared_writes.get(key) && (self.linearizable || writes.lower_bound(Bound::Excluded(&commit)).key().is_some()) {
+                    // Read conflicts with later prepared write.
+                    return PrepareResult::Abstain;
+                }
+            }
         }
+
+        // Check for conflicts with the write set.
+        for (key, write) in &transaction.write_set {
+            if let Some((timestamp, _)) = self.inner.get(key) {
+                // If the last commited write is after the write...
+                if self.linearizable && timestamp > commit {
+                    // ...then the write isn't linearizable.
+                    return PrepareResult::Retry {
+                        proposed: timestamp,
+                    };
+                }
+
+                // if last committed read is after the write...
+                let last_read = if self.linearizable {
+                    // Cannot write an old version.
+                    self.inner.get_last_read(key)
+                } else {
+                    // Might be able to write an old version.
+                    self.inner.get_last_read_at(key, commit)
+                };
+
+                if let Some(last_read) = last_read && last_read > commit {
+                    // Write conflicts with a later committed read.
+                    return PrepareResult::Retry{proposed: last_read};
+                }
+            }
+
+            if self.linearizable && let Some(writes) = prepared_writes.get(key) {
+                if let Some(write) = writes.lower_bound(Bound::Excluded(&commit)).key() {
+                    // Write conflicts with later prepared write.
+                    return PrepareResult::Retry { proposed: *write };
+                }
+            }
+
+            if let Some(reads) = prepared_reads.get(key) {
+                if reads.lower_bound(Bound::Excluded(&commit)).key().is_some() {
+                    // Write conflicts with later prepared read.
+                    return PrepareResult::Abstain;
+                }
+            }
+        }
+
+        self.prepared.insert(id, (commit, transaction));
 
         PrepareResult::Ok
     }
@@ -93,21 +167,21 @@ impl<K: Eq + Hash, V, TS: Copy + Ord> Store<K, V, TS> {
         self.inner.put(key, value, timestamp);
     }
 
-    pub(crate) fn prepared_reads(&self) -> HashMap<&K, BTreeSet<TS>> {
-        let mut ret: HashMap<&K, BTreeSet<TS>> = HashMap::default();
+    pub(crate) fn prepared_reads(&self) -> HashMap<&K, BTreeMap<TS, ()>> {
+        let mut ret: HashMap<&K, BTreeMap<TS, ()>> = HashMap::default();
         for (_, (timestamp, transaction)) in &self.prepared {
             for key in transaction.read_set.keys() {
-                ret.entry(key).or_default().insert(*timestamp);
+                ret.entry(key).or_default().insert(*timestamp, ());
             }
         }
         ret
     }
 
-    pub(crate) fn prepared_writes(&self) -> HashMap<&K, BTreeSet<TS>> {
-        let mut ret: HashMap<&K, BTreeSet<TS>> = HashMap::default();
+    pub(crate) fn prepared_writes(&self) -> HashMap<&K, BTreeMap<TS, ()>> {
+        let mut ret: HashMap<&K, BTreeMap<TS, ()>> = HashMap::default();
         for (_, (timestamp, transaction)) in &self.prepared {
             for key in transaction.write_set.keys() {
-                ret.entry(key).or_default().insert(*timestamp);
+                ret.entry(key).or_default().insert(*timestamp, ());
             }
         }
         ret
