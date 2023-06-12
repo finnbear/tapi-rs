0b566ae6c5aa79a539377758d629f83f174766e5
Finn Bear <finnbearlabs@gmail.com>
2023-06-12 13:35:58 -0700

    Implement TAPIR (WIP).

--- src/tapir/mod.rs
+++ src/tapir/mod.rs
@@ -1,8 +1,10 @@
+mod client;
 mod replica;
 mod reply;
 mod request;
 mod timestamp;
 
+pub(crate) use client::Client;
 pub(crate) use reply::Reply;
 pub(crate) use request::Request;
 pub(crate) use timestamp::Timestamp;
